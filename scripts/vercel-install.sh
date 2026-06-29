11055cf647314336a7e711e2a80ae781fe38a8e0
Kushal Samant <writetokushaldsamant@gmail.com>
2026-06-29 14:10:56 +0530

    Simplify vercel-install.sh

--- scripts/vercel-install.sh
+++ scripts/vercel-install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 set -eu
-DESIGN_SYSTEM_DIR="../platform-design-system"
-DESIGN_SYSTEM_REPO="https://github.com/kushalsamant/platform-design-system.git"
-if [ ! -f "${DESIGN_SYSTEM_DIR}/package.json" ]; then
-  git clone --depth 1 "${DESIGN_SYSTEM_REPO}" "${DESIGN_SYSTEM_DIR}"
-fi
-npm ci --prefix "${DESIGN_SYSTEM_DIR}"
-npm run build --prefix "${DESIGN_SYSTEM_DIR}"
 npm install
